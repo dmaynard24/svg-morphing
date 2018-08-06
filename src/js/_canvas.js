81ce40b2c9a6d6967adf5e678bfe75c927f54403
dmaynard24 <davemaynard24@gmail.com>
2018-08-05 22:36:02 -0400

    renamed canvas to morphing-canvas

--- src/js/_canvas.js
+++ src/js/_canvas.js
@@ -1,39 +1,39 @@
 (function() {
-    $('body').on('click touch', '.canvas', function() {
-        // reset canvas
-        $('.canvas .left-vertical').attr({
+    $('body').on('click touch', '.morphing-canvas', function() {
+        // reset morphing-canvas
+        $('.morphing-canvas .left-vertical').attr({
             'x': '0%',
             'width': '0%'
         });
-        $('.canvas .right-vertical').attr({
+        $('.morphing-canvas .right-vertical').attr({
             'x': '100%',
             'width': '0%'
         });
-        $('.canvas .top-horizontal').attr({
+        $('.morphing-canvas .top-horizontal').attr({
             'x': '0%',
             'height': '0%'
         });
-        $('.canvas .bottom-horizontal').attr({
+        $('.morphing-canvas .bottom-horizontal').attr({
             'y': '100%',
             'height': '0%'
         });
 
         var morphingTimeline = anime.timeline(),
-            duration = 500,
+            duration = 400,
             size = $(window).outerWidth() < 992 ? 40 : 80,
             rectWidth = (size / $(this).outerWidth()) * 100,
             rectHeight = (size / $(this).outerHeight()) * 100;
         
         morphingTimeline
             .add({
-                targets: '.canvas .left-vertical',
+                targets: '.morphing-canvas .left-vertical',
                 width: rectWidth,
                 easing: 'easeOutQuad',
                 duration: duration,
                 offset: 0
             })
             .add({
-                targets: '.canvas .right-vertical',
+                targets: '.morphing-canvas .right-vertical',
                 width: rectWidth,
                 x: 100 - rectWidth,
                 easing: 'easeOutQuad',
@@ -41,14 +41,14 @@
                 offset: 0
             })
             .add({
-                targets: '.canvas .top-horizontal',
+                targets: '.morphing-canvas .top-horizontal',
                 height: rectHeight,
                 easing: 'easeOutQuad',
                 duration: duration,
                 offset: 0
             })
             .add({
-                targets: '.canvas .bottom-horizontal',
+                targets: '.morphing-canvas .bottom-horizontal',
                 height: rectHeight,
                 y: 100 - rectHeight,
                 easing: 'easeOutQuad',
@@ -56,7 +56,7 @@
                 offset: 0
             })
             .add({
-                targets: '.canvas .left-vertical',
+                targets: '.morphing-canvas .left-vertical',
                 width: 100,
                 easing: 'easeOutQuad',
                 duration: duration,
